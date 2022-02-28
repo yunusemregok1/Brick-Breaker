4484bb1fa3bff02f574260004fa8743b9a17d6fa
Yunus emre gök <ynsemregok@outlook.com>
2022-02-28 20:09:37 +0300

    Add Paddle Scripts

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Rigidbody2D PaddleRb;
+    public Vector2 PaddleDirection;
+    public float PaddleSpeed = 50;
+
+    private void Awake()
     {
-        
+        PaddleRb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            PaddleDirection = Vector2.left;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            PaddleDirection = Vector2.right;
+        }
+        else
+        {
+            PaddleDirection = Vector2.zero;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (PaddleDirection != Vector2.zero)
+        {
+            PaddleRb.AddForce(PaddleDirection * PaddleSpeed);
+        }
     }
 }
